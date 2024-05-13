6ae6d1bede6406c2e5a55a09c79ea3583c1a259a
Anastilesnikova <anastacialesnikowa@gmail.com>
2024-05-14 01:12:21 +0300

    Добавление нескольких сообщений

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -5,5 +5,17 @@ import com.topic3.android.reddit.viewmodel.MainViewModel
 
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
+    val posts: List<PostModel> by viewModel.allPosts.observeAsState(listOf())
 
+    LazyColumn(
+        modifier = Modifier.background(color = MaterialTheme.colors.secondary)) {
+        items(posts) {
+            if (it.type == PostType.TEXT) {
+                TextPost(it)
+            } else {
+                ImagePost(it)
+            }
+            Spacer(modifier = Modifier.height(6.dp))
+        }
+    }
 }
